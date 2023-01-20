ad3a740707494edaf9c2d567a7ab7f0452c34abb
vinod <infovinodjangid@gmail.com>
2023-01-20 23:57:18 +0530

    countless commit

--- script.js
+++ script.js
@@ -21,46 +21,6 @@ function hamburgerMenu() {
   
 
 
-let majorDescription = document.getElementById("major-attraction-para");
-let attractionSlider = document.getElementById("attraction-slider");
-
- const list = document.querySelectorAll('.nav-list-tabs');
- list.forEach((item) =>
-item.addEventListener('click',activeTab));
-function activeTab(){
-    list.forEach((item) =>
-    item.classList.remove('active-tab'));
-    this.classList.add('active-tab');
-    if (this == document.getElementById("event")) {
-        majorDescription.textContent = "Events wala description"
-           attractionSlider.innerHTML = ` <div class="image-div" id="image1"><img id="sliderimage1" src="events1.jpg" alt=""></div>
-           <div class="image-div" id="image2"><img id="sliderimage2" src="events2.jpg" alt=""></div>
-           <div class="image-div" id="image3"><img id="sliderimage3" src="events3.jpg" alt=""></div>`
-    }
-    else if (this == document.getElementById("games")) {
-        majorDescription.textContent = "Games wala description"
-           attractionSlider.innerHTML = ` <div class="image-div" id="image1"><img id="sliderimage1" src="games1.jpg" alt=""></div>
-           <div class="image-div" id="image2"><img id="sliderimage2" src="games2.jpg" alt=""></div>`
-    }
-    else if (this == document.getElementById("workshops")) {
-        majorDescription.textContent = "Workshops wala description"
-           attractionSlider.innerHTML = ` <div class="image-div" id="image1"><img id="sliderimage1" src="workshops1.jpg" alt=""></div>
-           <div class="image-div" id="image2"><img id="sliderimage2" src="workshops2.jpg" alt=""></div>`
-    }
-    else if (this == document.getElementById("carnival")) {
-        majorDescription.textContent = "Carnival wala description"
-           attractionSlider.innerHTML = ` <div class="image-div" id="image1"><img id="sliderimage1" src="carnival1.jpg" alt=""></div>
-           <div class="image-div" id="image2"><img id="sliderimage2" src="carnival2.jpg" alt=""></div>
-           <div class="image-div" id="image3"><img id="sliderimage3" src="carnival3.jpg" alt=""></div>`
-    }
-    else if (this == document.getElementById("zonal")) {
-        majorDescription.textContent = "Zonal wala description"
-           attractionSlider.innerHTML = ` <div class="image-div" id="image1"><img id="sliderimage1" src="zonal.jpg" alt=""></div>`
-    }
-}
-
-
-
 let mybutton = document.getElementById("backtotopbutton");
 window.onscroll = function(){
   scrollFunction()
